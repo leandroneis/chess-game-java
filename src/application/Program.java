b1c2da0cc02560cbb42018afebee8a71f6e9d466
Leandro Gustavo Neis <lgn200@gmail.com>
2020-11-18 20:56:13 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
                     captured.add(capturedPiece);
                 }
 
+                if (chessMath.getPromoted() != null){
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMath.replacePromotedPiece(type.toUpperCase());
+                }
+
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
